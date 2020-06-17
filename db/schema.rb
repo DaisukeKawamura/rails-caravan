e1abde2e2009965e9328ed8315bf60ee521dad51
DaisukeKawamura <da1i9su9ke7.0715@icloud.com>
2020-06-17 15:06:03 +0900

    initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_25_102702) do
+ActiveRecord::Schema.define(version: 2020_04_27_100535) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.string "category"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
